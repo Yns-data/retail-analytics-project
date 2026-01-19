0d76921f3765096ac69f97fbc9ae65b6cdcbe5bd
yns-data <younesessoualhi@gmal.com>
2026-01-19 22:45:58 +0000

    commenting if __name__

--- data-source-api/main.py
+++ data-source-api/main.py
@@ -150,6 +150,6 @@ def get_articles_by_category(
         return JSONResponse(
             status_code=400,
             content={"error": str(e)})
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+# if __name__ == "__main__":
+#     import uvicorn
+#     uvicorn.run(app, host="0.0.0.0", port=8000)
