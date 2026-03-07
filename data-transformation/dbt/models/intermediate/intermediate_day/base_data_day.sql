6ed3d4700223038b8296d433ef1749acf9b95e79
yns-data <younesessoualhi@gmal.com>
2026-03-07 15:19:47 +0000

    adding dbt files

--- data-transformation/dbt/models/intermediate/intermediate_day/base_data_day.sql
+++ data-transformation/dbt/models/intermediate/intermediate_day/base_data_day.sql
@@ -2,7 +2,7 @@
 
 
 SELECT *,
-    (sum_visitors + sum_pages_viewed + sum_food_articles + sum_wear_articles + 
+        (sum_food_articles + sum_wear_articles + 
         sum_electronics_articles + sum_sports_articles + sum_toys_articles + 
         sum_home_articles + sum_garden_articles + sum_beauty_articles + 
         sum_automotive_articles) AS total_sum
